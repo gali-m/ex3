c5400532949ec91cf057ee60650e728deb295e16
Noa <noaro@campus.technion.ac.il>
2021-01-10 19:34:00 +0200

    exeption file

---
+++ exceptions.h
@@ -0,0 +1,20 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+// #include <iostream>
+// #include "date_wrap.h"
+
+
+
+namespace mtm {
+
+    class Exception : public std::exception {
+        class AlreadyRegistered {};
+
+
+
+    }
+}
+
+
+#endif //EXCEPTIONS_H
