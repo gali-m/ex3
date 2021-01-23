d3275f527d1a92c30b97d698e25ea1f6cc4f16c7
Noa <noaro@campus.technion.ac.il>
2021-01-23 19:37:11 +0200

    partA comments

--- partA/date_wrap.h
+++ partA/date_wrap.h
@@ -13,16 +13,28 @@ namespace mtm
     private:
         Date date;
         enum date_parameter {DAY,MONTH,YEAR};
-        static int get_date_parameter(Date date, enum date_parameter date_parameter);
+
+        /**
+        * getDateParameter: a static private method for getting one of the parameters of date - day or month or year.
+        *
+        * @param date - a date to get the parameter of.
+        * @param date_parameter - a parameter to get from the date.
+        * @return
+        * 	int of the parameter in the date.
+        *   if faile return -1
+        */
+        static int getDateParameter(Date date, enum date_parameter date_parameter);
 
     public:
         DateWrap(int day, int month, int year);
         DateWrap(const DateWrap& date_wrap);
         ~DateWrap();
-	    DateWrap& operator=(const DateWrap& date_wrap);
+        
         int day() const;
         int month() const;
         int year() const;
+
+	    DateWrap& operator=(const DateWrap& date_wrap);
         bool operator==(const DateWrap& date_wrap);
         bool operator!=(const DateWrap& date_wrap);
         bool operator>(const DateWrap& date_wrap);
